1c898535d721fbde0d9ad914d6c6831777b8d50d
Michal Przekop <przekop.michal@gmail.com>
2017-12-02 11:28:34 +0100

    CreatedDungeonGenerator

---
+++ Assets/Scripts/RoomGenerator/Room.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room 
+{
+    private int _neighboursCount;
+
+    private bool[] _openSides;
+    private int _posX, _posY;
+    public Room(int posX,int posY)
+    {
+        _posX = posX;
+        _posY = posY;
+        _openSides = new bool[4];
+        _neighboursCount = 0;
+    }
+
+    public int NeighbourCount
+    {
+        get { return _neighboursCount; }
+        //set { _neighboursCount = value; }
+    }
+
+    public int X
+    {
+        get { return _posX; }
+    }
+
+    public int Y
+    {
+        get { return _posY; }
+    }
+}
---
+++ Assets/Scripts/RoomGenerator/RoomGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Utils;
+using UnityEngine;
+
+public class RoomGenerator : MonoBehaviour
+{
+    private bool[,] _map;
+    private List<Room> _rooms;
+    [SerializeField]
+    private int _dungeonSize;
+    private int _posX, _posY;
+    [SerializeField]
+    private GameObject _testRoom;
+	// Use this for initialization
+	void Start ()
+	{
+	    _map = new bool[15, 15];
+	    _map[7, 7] = true; //settining middle
+	    _posX = _posY = 7;
+        _rooms = new List<Room>();
+        _rooms.Add(new Room(7,7));
+        GenerateMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GenerateMap()
+    {
+        for (int i = 0; i < _dungeonSize; i++)
+        {
+            var choosenRoom = _rooms[Random.Range(0, _rooms.Count)];
+            _posY = choosenRoom.Y;
+            _posX = choosenRoom.X;
+            while (!IsFreePlaceArrond())
+            {
+                choosenRoom = _rooms[Random.Range(0, _rooms.Count)];
+                _posY = choosenRoom.Y;
+                _posX = choosenRoom.X;
+            }
+            CreateRoom();
+        }
+        foreach (var r in _rooms)
+        {
+            Instantiate(_testRoom, new Vector3(16*r.X, 0, 16*r.Y), _testRoom.transform.rotation);
+        }
+    }
+
+    bool IsFreePlaceArrond()
+    {
+        int neighboursCount = 0;
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == j||i==-1*j) continue;
+                
+                if (_posX + i == _map.GetLength(0) || _posX + i < 0 || _posY + j == _map.GetLength(1) ||
+                    _posY + j < 0||_map[_posX + i, _posY + j]) neighboursCount++;
+               
+            }
+        }
+        return neighboursCount != 4;
+    }
+
+    void CreateRoom()
+    {
+        while (true)
+        {
+            var choice = Random.Range(0, 4);
+            int newX = 0, newY = 0;
+            switch (choice)
+            {
+                case (int) Directions.UP:
+                    newX = _posX;
+                    newY = _posY + 1;
+                    break;
+                case (int) Directions.DOWN:
+                    newX = _posX;
+                    newY = _posY - 1;
+                    break;
+                case (int) Directions.LEFT:
+                    newX = _posX - 1;
+                    newY = _posY;
+                    break;
+                case (int) Directions.RIGHT:
+                    newX = _posX + 1;
+                    newY = _posY;
+                    break;
+            }
+            if (newX>=_map.GetLength(0)||newX<0||newY>=_map.GetLength(1)||newY<0|| _map[newX, newY])
+            {
+            }
+            else
+            {
+                _map[newX, newY] = true;
+                _rooms.Add(new Room(newX, newY));
+                if (_rooms.Count % 3 == 0)
+                {
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            if (newX + i >= _map.GetLength(0) || newX + i < 0 || newY + j >= _map.GetLength(1) ||
+                                newY + j < 0) continue;
+                            if (i == j || i == -1 * j)
+                            {
+                                _map[newX+i, newY+j] = true;
+                            }
+                        }
+                    }
+                    
+                }
+                return;
+            }
+        }
+    }
+}
