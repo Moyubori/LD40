ddc308e8dce2c7b0b59e593f0fa8a6d37038ad36
Tymoteusz Buczkowski <moyubori@gmail.com>
2017-12-02 08:48:04 +0100

    Init

---
+++ Assets/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed = 10f;
+
+    private bool collision_happened = false;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("dd");
+        collision_happened = true;
+    }
+    private void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (collision_happened) {
+            collision_happened = false;
+        } else {
+            transform.position += new Vector3(horizontal * movementSpeed, vertical * movementSpeed, 0);
+        }
+    }
+
+}
