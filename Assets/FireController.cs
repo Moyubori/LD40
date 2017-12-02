8542a4510424834432b438445d2fc6c9f8ab6a3c
Tymoteusz Buczkowski <moyubori@gmail.com>
2017-12-02 11:24:38 +0100

    Rotation using controller input or mouse position

--- Assets/FireController.cs
+++ Assets/FireController.cs
@@ -7,17 +7,43 @@ public class FireController : MonoBehaviour {
 	[SerializeField]
 	private float rotationOffset = 0f;
 
-	private void Update () {
-		RotateToMousePos ();
+	private float prevAngle;
+
+	private void Awake() {
+		prevAngle = 0;
 	}
 
-	private void RotateToMousePos() {
+	private void Update () {
+		float horizontalC = Input.GetAxis ("xFireController");
+		float verticalC = Input.GetAxis ("yFireController");
+		bool mouseMoved = (Input.GetAxis ("mouseX") != 0) || (Input.GetAxis ("mouseY") != 0);
+		float angle = prevAngle;
+		if (mouseMoved) {
+			angle = AngleToMousePos ();
+		} else if ((horizontalC != 0) || (verticalC != 0)) {
+			angle = AngleToControllerInput (horizontalC, verticalC);
+		}
+		prevAngle = angle;
+		Rotate (angle);
+	}
+
+	private float AngleToMousePos() {
 		Vector3 inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		inputPos.Set (inputPos.x, inputPos.y, 0);
-		Debug.DrawLine (transform.position, inputPos);
+		Debug.DrawLine (transform.position, inputPos, Color.magenta);
 		Vector3 inputPosDiff = inputPos - transform.position;
 		inputPosDiff.Normalize ();
-		float angle = Mathf.Atan2(inputPosDiff.y, inputPosDiff.x) * Mathf.Rad2Deg - 90;
+		return Mathf.Atan2(inputPosDiff.y, inputPosDiff.x) * Mathf.Rad2Deg - 90;
+	}
+
+	private float AngleToControllerInput(float x, float y) {
+		Vector3 inputPosDiff = new Vector3(x, y, transform.position.z);
+		Debug.DrawLine (transform.position, transform.position + inputPosDiff, Color.green);
+		inputPosDiff.Normalize ();
+		return Mathf.Atan2(inputPosDiff.y, inputPosDiff.x) * Mathf.Rad2Deg - 90;
+	}
+
+	private void Rotate(float angle) {
 		transform.rotation = Quaternion.Euler (0, 0, angle + rotationOffset);
 	}
 
