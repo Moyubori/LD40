1d5827202de119ed3d402c34410bc04e92da027b
Tymoteusz Buczkowski <moyubori@gmail.com>
2017-12-02 17:02:01 +0100

    Shooting only when the controller stick is maximally tilted

---
+++ Assets/Scripts/AmmoCollectible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoCollectible : MonoBehaviour {
+
+	public float rotationSpeed = 150f;
+
+	private void Update() {
+		transform.rotation = Quaternion.Euler (new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + (rotationSpeed * Time.deltaTime), transform.rotation.eulerAngles.z));
+	}
+
+}
